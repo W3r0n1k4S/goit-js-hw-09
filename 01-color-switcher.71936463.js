31d2ae52098a045f9075db47d629c7eb1893c606
W3r0n1k4S <143906541+W3r0n1k4S@users.noreply.github.com>
2024-03-17 10:51:45 +0000

    Deploying to gh-pages from @ W3r0n1k4S/goit-js-hw-09@36aa3a7aa44f2c36f191675d952152a89b9e8f09 🚀

---
+++ 01-color-switcher.71936463.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval(o,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));let n=null;function o(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
+//# sourceMappingURL=01-color-switcher.71936463.js.map
